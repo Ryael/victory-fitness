60920a6f90a623a99f873a7554f2fab37185fc76
Andrey Belyakov <rayrah@live.com>
2024-03-05 22:18:29 +0000

    Adjust coaching scroll to change depending on color of nav bar.y

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,12 +101,19 @@ $(document).ready(function() {
 
 /* Coaching Navigation */
 
-/* Scoll to coaching comparisons. */
+/* Scoll to coaching comparisons depending on whether the navigation bar is present or not. */
 $(".coaching-scroll").click(function() {
-    $('html, body').animate({
+    if ($("#nav-bar.scroll").css("background") == "rgb(255, 255, 255)") {
+      $('html, body').animate({
+        scrollTop: $("#coaching-comparison-tabs").offset().top - 65},
+        'slow');
+    } else {
+      $('html, body').animate({
         scrollTop: $("#coaching-comparison-tabs").offset().top},
         'slow');
-});
+    }
+  }
+);
 
 /* Remove active classes from the tab and tab-pane. */
 function removeActiveClass() {
